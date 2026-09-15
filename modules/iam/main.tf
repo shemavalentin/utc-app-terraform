36fa2464d77c397bf84add4ea8bbfe555bbad056
Shema N. Valentin <shemavalentin@gmail.com>
2026-09-16 01:14:43 +0200

    updated the IAM so EC2 instances have permissions to publish logs to CloudWatch

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -17,18 +17,24 @@ resource "aws_iam_role" "app_role" {
   tags = merge(var.tags, { Name = "${var.project_name}-ec2-role" })
 }
 
-# 2. Managed Policy: AWS Systems Manager (SSM) for secure shell access without SSH keys
+# 2. SSM Policy for secure management (No SSH keys required)
 resource "aws_iam_role_policy_attachment" "ssm_core" {
   role       = aws_iam_role.app_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-# 3. Least-Privilege Custom Policy: S3 Application Bucket Access
+# 3. CloudWatch Agent Policy for log pushing & custom metrics
+resource "aws_iam_role_policy_attachment" "cloudwatch_agent" {
+  role       = aws_iam_role.app_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
+# 4. S3 Bucket Access Policy
 data "aws_iam_policy_document" "s3_access" {
   statement {
-    sid       = "S3AppBucketAccess"
-    effect    = "Allow"
-    actions   = [
+    sid    = "S3AppBucketAccess"
+    effect = "Allow"
+    actions = [
       "s3:GetObject",
       "s3:PutObject",
       "s3:ListBucket",
@@ -42,9 +48,8 @@ data "aws_iam_policy_document" "s3_access" {
 }
 
 resource "aws_iam_policy" "s3_access" {
-  name        = "${var.project_name}-s3-access-policy"
-  description = "Least-privilege policy for application S3 bucket access"
-  policy      = data.aws_iam_policy_document.s3_access.json
+  name   = "${var.project_name}-s3-access-policy"
+  policy = data.aws_iam_policy_document.s3_access.json
 }
 
 resource "aws_iam_role_policy_attachment" "s3_access" {
@@ -52,20 +57,12 @@ resource "aws_iam_role_policy_attachment" "s3_access" {
   policy_arn = aws_iam_policy.s3_access.arn
 }
 
-# 4. IAM Instance Profile (to attach to the EC2 Launch Template)
-resource "aws_iam_instance_profile" "app_profile" {
-  name = "${var.project_name}-ec2-instance-profile"
-  role = aws_iam_role.app_role.name
-
-  tags = merge(var.tags, { Name = "${var.project_name}-instance-profile" })
-}
-
-# Read permissions for Secrets Manager
+# 5. Secrets Manager Access Policy
 data "aws_iam_policy_document" "secrets_access" {
   statement {
-    sid       = "AllowReadAppSecrets"
-    effect    = "Allow"
-    actions   = ["secretsmanager:GetSecretValue", "secretsmanager:DescribeSecret"]
+    sid    = "AllowReadAppSecrets"
+    effect = "Allow"
+    actions = ["secretsmanager:GetSecretValue", "secretsmanager:DescribeSecret"]
     resources = [var.secret_arn]
   }
 }
@@ -78,4 +75,12 @@ resource "aws_iam_policy" "secrets_access" {
 resource "aws_iam_role_policy_attachment" "secrets_access" {
   role       = aws_iam_role.app_role.name
   policy_arn = aws_iam_policy.secrets_access.arn
+}
+
+# 6. IAM Instance Profile for Launch Template
+resource "aws_iam_instance_profile" "app_profile" {
+  name = "${var.project_name}-ec2-instance-profile"
+  role = aws_iam_role.app_role.name
+
+  tags = merge(var.tags, { Name = "${var.project_name}-instance-profile" })
 }
